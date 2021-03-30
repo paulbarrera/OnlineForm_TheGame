cac286e2db5e67b06d052792b90ad29765bd3471
Brittney <3944682+bawilc01@users.noreply.github.com>
2021-03-30 00:38:10 -0400

    Update main.py
    fixed comma issue in question_prompts throwing "index out of range" error; converted score variable to str that was throwing error about int variables in print statements not being supported; removed extra new line characters that were showing up in the new lines beneath multiple choice options as text

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ time.sleep(3)
 print("BEGIN")
 
 question_prompts = [
-    "Who said, Luke, I am your father\n(a) Han Solo\n(b) Emperor\n(c) Darth Vader\n\\n",
-    "Which celeb said this about COVID-19: It's a virus. I get it, I respect it\n(a) Vanessa Hudgens\n(b) Madonna\n(c) Miley Cyrus\n\\n",
-    "Which Kardashian owns a sock line\n(a) Khloe\n(b) Rob\n(c) Karen\n\\n",
-    "Who was the first Disney Princess\n(a) Snow White\n(b) Cinderella\n(c) Aurora\n\\n"
-    "Who lived in America before the Europeans arrived\n(a) Romulans\n(b) American Indians\n(c) No One\n\\n",
-    "Who is Miley Cyrus godmother\n(a) Loretta Lynn\n(b) Reba McEntire\n(c) Dolly Parton\n\\n",
-    "Which Famous President was born in Kentucky\n(a) Andrew Jackon\n(b) Thomas Jefferson\n(c) Abraham Lincoln\n\\n",
-    "Who directed Jurassic Park\n(a) Martin Scorcese\n(b) George Lucas\n(C) Steven Spielberg\n\\n",
-    "Who produces the Real Housewives shows\n(a) Andy Cohen\n(b) Mark Burnett\n(c) Simon Cowell\n\\n",
-    "Yearly Americans eat 3 billion of these\n(a) Hot Dogs\n(b) Pizza Slices\n(c) Tacos\n\\n",
+    "Who said, Luke, I am your father\n(a) Han Solo\n(b) Emperor\n(c) Darth Vader\n",
+    "Which celeb said this about COVID-19: It's a virus. I get it, I respect it\n(a) Vanessa Hudgens\n(b) Madonna\n(c) Miley Cyrus\n",
+    "Which Kardashian owns a sock line\n(a) Khloe\n(b) Rob\n(c) Karen\n",
+    "Who was the first Disney Princess\n(a) Snow White\n(b) Cinderella\n(c) Aurora\n"
+    "Who lived in America before the Europeans arrived\n(a) Romulans\n(b) American Indians\n(c) No One\n",
+    "Who is Miley Cyrus godmother\n(a) Loretta Lynn\n(b) Reba McEntire\n(c) Dolly Parton\n",
+    "Which Famous President was born in Kentucky\n(a) Andrew Jackon\n(b) Thomas Jefferson\n(c) Abraham Lincoln\n",
+    "Who directed Jurassic Park\n(a) Martin Scorcese\n(b) George Lucas\n(C) Steven Spielberg\n",
+    "Who produces the Real Housewives shows\n(a) Andy Cohen\n(b) Mark Burnett\n(c) Simon Cowell\n",
+    "Yearly Americans eat 3 billion of these\n(a) Hot Dogs\n(b) Pizza Slices\n(c) Tacos\n",
 ]
 
 #Create a dictionary or list. 
@@ -55,7 +55,7 @@ def run_test(questions):
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
-    print("you got ", score + "out of", len(questions))
+    print("you got ", str(score) + " out of", len(questions))
 
 run_test(questions)
 
